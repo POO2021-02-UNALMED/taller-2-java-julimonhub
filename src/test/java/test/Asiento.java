7e2333b8ba41f828af00a16abfd547381e467e6b
julimonhub <92111604+julimonhub@users.noreply.github.com>
2021-10-19 20:24:14 -0500

    update4

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,14 +1,12 @@
 package test;
-
 public class Asiento {
 	    public String color;
 	    public int precio;
 	    public int registro;
 	
 	    public void cambiarColor(String color) {
-		if (color.equals("rojo") || color.equals("verde") || color.equals("amarillo") || color.equals("negro") || color.equals("blanco")){
-			     this.color = color;
-			     
-		}
-	}
+		         if (color.equals("rojo") || color.equals("verde") || color.equals("amarillo") || color.equals("negro")|| color.equals("blanco")) {
+			              this.color = color;
+		         }			
+	     }
 }
